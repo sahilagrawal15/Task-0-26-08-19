da92d995c7bff839e1cfac8175fd9bb674ad3398
Sahil <Sahil@LAPTOP-JASVJJ2C>
2019-08-27 10:17:03 +0530

    first repo

---
+++ src/testjar/Check.java
@@ -0,0 +1,15 @@
+package testjar;
+
+import sample.Abc;
+
+public class Check {
+
+	public static void main(String[] args) {
+		
+		Abc a=new Abc();
+		System.out.println("Hello");
+		System.out.println(a.product(10, 13));
+		
+	}
+
+}
